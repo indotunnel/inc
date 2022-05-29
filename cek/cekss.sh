0c06f3d689d4a7ae03ac52dc5194365d00e2a5a5
Indotunnel <100355086+indotunnel@users.noreply.github.com>
2022-05-30 00:59:50 +0700

    Add files via upload

---
+++ cek/cekss.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# My Telegram : https://t.me/Manternet
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+echo "----------------------------------";
+echo "----=[ SS - OBFS User Login ]=----";
+echo "----------------------------------";
+echo ""
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+x=1
+echo "----------------------------------";
+echo " User | TLS"
+echo "----------------------------------";
+for akun in "${data[@]}"
+do
+port=$(cat /etc/shadowsocks-libev/akun.conf | grep '^port_tls' | cut -d ' ' -f 2 | tr '\n' ' ' | awk '{print $'"$x"'}')
+jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w $port | awk '{print $2}' | sort | uniq | nl)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+echo " $akun - $port"
+echo "$jum";
+echo "----------------------------------"
+fi
+x=$(( "$x" + 1 ))
+done
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+x=1
+echo ""
+echo "----------------------------------";
+echo " User |  No TLS"
+echo "----------------------------------";
+for akun in "${data[@]}"
+do
+port=$(cat /etc/shadowsocks-libev/akun.conf | grep '^port_http' | cut -d ' ' -f 2 | tr '\n' ' ' | awk '{print $'"$x"'}')
+jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w $port | awk '{print $2}' | sort | uniq | nl)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+echo " $akun - $port"
+echo "$jum";
+echo "----------------------------------"
+fi
+x=$(( "$x" + 1 ))
+done
---
+++ cek/member.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# My Telegram : https://t.me/Mardhex
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+echo "---------------------------------------------------"
+echo "USERNAME          EXP DATE          STATUS"
+echo "---------------------------------------------------"
+while read expired
+do
+AKUN="$(echo $expired | cut -d: -f1)"
+ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+status="$(passwd -S $AKUN | awk '{print $2}' )"
+if [[ $ID -ge 1000 ]]; then
+if [[ "$status" = "L" ]]; then
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}LOCKED${NORMAL}"
+else
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}UNLOCKED${NORMAL}"
+fi
+fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo "---------------------------------------------------"
+echo "Account number: $JUMLAH user"
+echo "---------------------------------------------------"
