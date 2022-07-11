fd88db9a375d8f5e96201e0c2e2ff92a247c6b9e
Indotunnel <100355086+indotunnel@users.noreply.github.com>
2022-07-11 21:55:52 +0700

    Add files via upload

--- setup/vpn.sh
+++ setup/vpn.sh
@@ -57,12 +57,12 @@ END
 
 sed -i $MYIP2 /etc/openvpn/tcp.ovpn;
 
-# Buat config client UDP 2200
+# Buat config client UDP 1194
 cat > /etc/openvpn/udp.ovpn <<-END
 client
 dev tun
 proto udp
-remote xxxxxxxxx 2200
+remote xxxxxxxxx 1194
 resolv-retry infinite
 route-method exe
 nobind
@@ -80,7 +80,7 @@ cat > /etc/openvpn/ssl.ovpn <<-END
 client
 dev tun
 proto tcp
-remote xxxxxxxxx 990
+remote xxxxxxxxx 1196
 resolv-retry infinite
 route-method exe
 nobind
@@ -105,12 +105,12 @@ echo '</ca>' >> /etc/openvpn/tcp.ovpn
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 1194 )
 cp /etc/openvpn/tcp.ovpn /home/vps/public_html/tcp.ovpn
 
-# masukkan certificatenya ke dalam config client UDP 2200
+# masukkan certificatenya ke dalam config client UDP 1194
 echo '<ca>' >> /etc/openvpn/udp.ovpn
 cat /etc/openvpn/server/ca.crt >> /etc/openvpn/udp.ovpn
 echo '</ca>' >> /etc/openvpn/udp.ovpn
 
-# Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 2200 )
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 1194 )
 cp /etc/openvpn/udp.ovpn /home/vps/public_html/udp.ovpn
 
 # masukkan certificatenya ke dalam config client SSL
