f340b4892fbc57bbde8744c962ea925d118f2c79
Indotunnel <100355086+indotunnel@users.noreply.github.com>
2022-07-11 20:52:58 +0700

    Update ins-xray.sh

--- setup/ins-xray.sh
+++ setup/ins-xray.sh
@@ -762,7 +762,7 @@ cat > /etc/xray/vmessgrpc.json << END
     },
     "inbounds": [
         {
-            "port": 880,
+            "port": 80,
             "protocol": "vmess",
             "settings": {
                 "clients": [
@@ -812,7 +812,7 @@ cat > /etc/xray/vlessgrpc.json << END
     },
     "inbounds": [
         {
-            "port": 443,
+            "port": 880,
             "protocol": "vless",
             "settings": {
                 "clients": [
