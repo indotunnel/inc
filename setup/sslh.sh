8b6e0cf04f324c6607e5d0ad949b9c1318985df5
Indotunnel <100355086+indotunnel@users.noreply.github.com>
2022-07-12 10:10:08 +0700

    Update sslh.sh

--- setup/sslh.sh
+++ setup/sslh.sh
@@ -5,7 +5,7 @@ RUN=yes
 
 DAEMON=/usr/sbin/sslh
 
-DAEMON_OPTS='--user sslh --listen 0.0.0.0:443 --ssh 127.0.0.1:143 --ssl 127.0.0.1:3127 --http 127.0.0.1:8080 --openvpn 127.0.0.1:1196 --pidfile /var/run/sslh/sslh.pid'
+DAEMON_OPTS='--user sslh --listen 0.0.0.0:443 --ssh 127.0.0.1:143 --ssl 127.0.0.1:3127 --http 127.0.0.1:8080 --openvpn 127.0.0.1:1194 --pidfile /var/run/sslh/sslh.pid'
 EOF
 
 systemctl enable sslh
